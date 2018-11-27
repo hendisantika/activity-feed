51171cd4968355e9c3fb3ba6a12a15eb31fa9fb9
Hendi Santika <hendisantika@gmail.com>
2018-11-27 07:01:40 +0700

    #1 - Initial Project

---
+++ src/main/java/com/hendisantika/activityfeed/ActivityFeedApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.activityfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityFeedApplication.class, args);
+    }
+}
